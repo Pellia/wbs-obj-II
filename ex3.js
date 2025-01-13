3ac65ca4a62cc6999c31f5f63a2fd6829ffbfcc4
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-01-13 15:28:09 +0100

    finish ex3

--- ex3.js
+++ ex3.js
@@ -26,3 +26,57 @@ class Book extends LibraryItem {
         return super.getInfo() + `, Pages: ${this.#pages}`;
     }
 }
+
+// Member
+class Member {
+    #name;
+    #booksCheckedOut;
+
+    constructor(name, booksCheckedOut = []) {
+        this.#name = name;
+        this.#booksCheckedOut = booksCheckedOut;
+    }
+
+    checkOutBook(book) {
+        this.#booksCheckedOut.push(book);
+    }
+
+    returnBook(book) {
+        const index = this.#booksCheckedOut.indexOf(book);
+        if (index > -1) {
+            this.#booksCheckedOut.splice(index, 1);
+        }
+    }
+
+    listBooks() {
+        if (this.#booksCheckedOut.length === 0) {
+            console.log("No Books checked out");
+        } else {
+            console.log(`Listing checked out Books from ${this.getName()} ...`);
+            for (let i = 0; i < this.#booksCheckedOut.length; i++) {
+                console.log(this.#booksCheckedOut[i].getInfo());
+            }
+        }
+    }
+
+    getName() {
+        return this.#name;
+    }
+}
+
+// Create different Book Objects
+const book1 = new Book("Tomorrow, and Tomorrow, and Tomorrow", "Gabrielle Zevin", 416, "No");
+const book2 = new Book("Demon Slayer: Kimetsu no Yaiba", "Koyoharu Gotouge", 4496, "Yes");
+const book3 = new Book("The Pragmatic Programmer", "Andy Hunt and Dave Thomas", 320, "Yes");
+const book4 = new Book("Atomic Habits", "James Clear", 320, "Yes");
+const book5 = new Book("Tubes: Behind the Scenes at the Internet", "Andrew Blum", 281, "No");
+
+// Create Members and check out Books
+const m1 = new Member("Max");
+const m2 = new Member("Daniel");
+m1.checkOutBook(book1);
+m1.checkOutBook(book2);
+m1.listBooks();
+m1.returnBook(book2);
+m1.listBooks();
+m2.listBooks();
